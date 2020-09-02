bcaa4cacfefcc00575c6abd92b691d8888f4cdeb
AmitRajput15 <70574936+AmitRajput15@users.noreply.github.com>
2020-09-02 16:48:03 +0530

    Update JenkinsCalc.java

--- src/main/java/com/amit_test/jenkins/JenkinsCalc.java
+++ src/main/java/com/amit_test/jenkins/JenkinsCalc.java
@@ -3,7 +3,7 @@ package com.amit_test.jenkins;
 public class JenkinsCalc {
 
 	public int add(int a, int b) {
-		System.out.println("-------*****Testing Jenkins Integeration with maven*******-------");
+		System.out.println("-------*****Testing Jenkins Integeration with git repo maven*******-------");
 		return a+b;
 	}
 	public int subtract(int a, int b) {
